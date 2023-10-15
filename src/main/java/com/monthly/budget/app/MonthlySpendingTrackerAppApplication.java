985e146afeccbb561dba545d2106505160e67502
RodrigoSoaresNascimento <rodrigosnascimento1@gmail.com>
2023-10-15 15:20:24 -0300

    add a docker spring boot jar

--- src/main/java/com/monthly/budget/app/MonthlySpendingTrackerAppApplication.java
+++ src/main/java/com/monthly/budget/app/MonthlySpendingTrackerAppApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MonthlySpendingTrackerAppApplication {
 
+	public String PORT = System.getenv("PORT");
+
 	public static void main(String[] args) {
 		SpringApplication.run(MonthlySpendingTrackerAppApplication.class, args);
 	}
